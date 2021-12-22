1573dd05ef2350c83e3108a3983c54a8cd6a7ce2
Joshua Weber <josh@joshuaw.de>
2021-12-22 10:26:14 +0100

    🧪 Add buildpack check
    Signed-off-by: Niklas Lehnfeld <niklas@lehnfeld.net>
    Co-authored-by: Niklas Lehnfeld <niklas@lehnfeld.net>

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const RELEASE_TO_FILTER = '4.47';
+
+const App = () => {
+  
+  const [isReleased, setReleased] = React.useState<boolean>(false);
+
+  const fetchBuildpacks = async () => {
+    const resp = (await axios.get('https://api.github.com/repos/cloudfoundry/java-buildpack-release/releases')).data;
+    const releases = resp.filter((release: {name: string}) => release.name === RELEASE_TO_FILTER);
+    setReleased(releases.length > 0);
+  };
+
+  useEffect(() => {
+    fetchBuildpacks();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Is java-buildpack v{RELEASE_TO_FILTER} released yet?
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://bosh.io/releases/github.com/cloudfoundry/java-buildpack-release?all=1v"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {isReleased ? 'Yes' : 'No'}
         </a>
       </header>
     </div>
